2634fdb27430c034084d2d7e47a4fceca558e942
Sojeong Park <macpark09@gmail.com>
2022-02-11 13:40:48 +0000

    initial development branch

--- src/components/Opps.svelte
+++ src/components/Opps.svelte
@@ -1,22 +1,11 @@
 <script>
-    export let opps =[];
 
 </script>
 
-<div class="opp-list">
-    {#each opps as opp (opp.id)}
-    <div>
-    <li>{opp.name}</li>
-    </div>
-    {/each}
+<div>
 
 </div>
 
 <style>
-    li {
-        list-style: none;
-        padding:5px;
-    
-    }
-    
+   
 </style>
